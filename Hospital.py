498946765119555102687e03ecc7139216a7e5b6
Tyler Hynes <tyler.c.hynes@gmail.com>
2022-12-12 20:19:50 -0700

    Updated Managment.py

--- Hospital.py
+++ Hospital.py
@@ -21,148 +21,143 @@ class Doctor:
     def __str__(self):
         return f'{self.name} with employee id{self.id} is a doctor.'
 
+    def formatDrInfo(propertiesValuesList):
+        spaces = [5, 23, 16, 16, 16, 12]
+        formattedText = ""
 
-def formatDrInfo(propertiesValuesList):
-    spaces = [5, 23, 16, 16, 16, 12]
-    formattedText = ""
+        for item in propertiesValuesList:
+            formattedText += item + \
+                (" " * (spaces[propertiesValuesList.index(item)] - len(item)))
+        return formattedText
 
-    for item in propertiesValuesList:
-        formattedText += item + \
-            (" " * (spaces[propertiesValuesList.index(item)] - len(item)))
-    return formattedText
+    def enterDrInfo(self):
+        self.id = input("Enter the Doctor's ID: \n")
+        self.name = input("Enter the Doctor's name: \n")
+        self.specialization = input("Enter the Doctor's specialty: \n")
+        self.workingTime = input(
+            "Enter the Doctor's timing (e.g., 7am-10pm): \n")
+        self.qualification = input("Enter the Doctor's qualification: \n")
+        self.roomNumber = input("Enter the Doctor's room number: \n")
 
+        self.addDrToFile(self)
 
-def enterDrInfo(self):
-    self.id = input("Enter the Doctor's ID: \n")
-    self.name = input("Enter the Doctor's name: \n")
-    self.specialization = input("Enter the Doctor's specialty: \n")
-    self.workingTime = input("Enter the Doctor's timing (e.g., 7am-10pm): \n")
-    self.qualification = input("Enter the Doctor's qualification: \n")
-    self.roomNumber = input("Enter the Doctor's room number: \n")
+    def readDoctorsFile():
+        path = "data\doctors.txt"
+        doctorsObjectList = []
+        try:
+            file = open(path, 'r')
+            lines = file.readlines()
+            for line in lines:
+                if line.replace(" ", "") != "\n":
+                    line = line.replace('\n', '')
+                    line = re.split(r'\s{2,}', line)
+                    doctor = Doctor(line[0], line[1], line[2],
+                                    line[3], line[4], line[5])
 
-    self.addDrToFile(self)
+                    doctorsObjectList.append(doctor)
 
+            file.close()
+        except IOError:
+            file = open(path, 'a+')
+            print("doctors.txt file created")
 
-def readDoctorsFile():
-    path = "data\doctors.txt"
-    doctorsObjectList = []
-    try:
-        file = open(path, 'r')
-        lines = file.readlines()
-        for line in lines:
-            if line.replace(" ", "") != "\n":
-                line = line.replace('\n', '')
-                line = re.split(r'\s{2,}', line)
-                doctor = Doctor(line[0], line[1], line[2],
-                                line[3], line[4], line[5])
+        return doctorsObjectList
 
-                doctorsObjectList.append(doctor)
+    def searchDoctorById(idSearch):
+        doctorsObjectList = Doctor.readDoctorsFile()
+        idExist = False
+        for doctor in doctorsObjectList:
+            if doctor.id == idSearch:
+                doctor.displayDoctorInfo()
+                idExist = True
+                return doctorsObjectList.index(doctor)
+        if idExist == False:
+            print("Can't find the doctor with the same ID on the system \n")
+            return -1
 
+    def searchDoctorByName(nameSearch):
+        doctorsObjectList = Doctor.readDoctorsFile()
+        nameExist = False
+
+        for doctor in doctorsObjectList:
+            if doctor.name == nameSearch:
+                doctor.displayDoctorInfo()
+                nameExist = True
+        if nameExist == False:
+            print("Can't find the doctor with the same name on the system \n")
+            return -1
+
+    def displayDoctorInfo(self):
+        headerList = ["ID", "Name", "Specialty",
+                      "Timing", "Qualification", "Room Number"]
+        print(Doctor.formatDrInfo(headerList) + "\n")
+        valuesList = [self.id, self.name, self.specialization,
+                      self.workingTime, self.qualification, self.roomNumber]
+        print(Doctor.formatDrInfo(valuesList))
+
+    def readDoctorsFile():
+        path = "data\doctors.txt"
+        doctorsObjectList = []
+        try:
+            file = open(path, 'r')
+            lines = file.readlines()
+            for line in lines:
+                if line.replace(" ", "") != "\n":
+                    line = line.replace('\n', '')
+                    line = re.split(r'\s{2,}', line)
+                    doctor = Doctor(line[0], line[1], line[2], line[3], line[4], line[5])
+
+                    doctorsObjectList.append(doctor)
+
+            file.close()
+        except IOError:
+            file = open(path, 'a+')
+            print("doctors.txt file created")
+
+        return doctorsObjectList
+
+    def displayDoctorsList():
+        path = "data\doctors.txt"
+        headerList = ["ID", "Name", "Specialty",
+                      "Timing", "Qualification", "Room Number"]
+        headerSpaces = [5, 23, 16, 16, 16, 12]
+        for item in headerList:
+            print(
+                item + (" " * (headerSpaces[headerList.index(item)] - len(item))), end="")
+        print("\n")
+        with open(path, 'r') as file:
+            lines = file.readlines()
+            for line in lines:
+                print(line)
         file.close()
-    except IOError:
-        file = open(path, 'a+')
-        print("doctors.txt file created")
 
-    return doctorsObjectList
+    def writeListOfDoctorsToFile(doctorsObjectList):
+        path = "data\doctors.txt"
+        file = open(path, "r+")
+        textOutput = ""
+        for dr in doctorsObjectList:
+            drProperties = [dr.id, dr.name, dr.specialization,
+                            dr.workingTime, dr.qualification, dr.roomNumber]
+            ft = Doctor.formatDrInfo(drProperties)
+            textOutput += ft + "\n\n"
 
+        file.truncate(0)
+        file.write(textOutput)
+        file.close()
 
-def searchDoctorById(idSearch):
-    doctorsObjectList = Doctor.readDoctorsFile()
-    idExist = False
-    for doctor in doctorsObjectList:
-        if doctor.id == idSearch:
-            doctor.displayDoctorInfo()
-            idExist = True
-            return doctorsObjectList.index(doctor)
-    if idExist == False:
-        print("Can't find the doctor with the same ID on the system \n")
-        return -1
+    def addDrToFile(drObject):
+        path = "data\doctors.txt"
+        textOutput = ""
 
-
-def searchDoctorByName(nameSearch):
-    doctorsObjectList = Doctor.readDoctorsFile()
-    nameExist = False
-
-    for doctor in doctorsObjectList:
-        if doctor.name == nameSearch:
-            doctor.displayDoctorInfo()
-            nameExist = True
-    if nameExist == False:
-        print("Can't find the doctor with the same name on the system \n")
-        return -1
-
-
-def displayDoctorInfo(self):
-    headerList = ["ID", "Name", "Specialty",
-                  "Timing", "Qualification", "Room Number"]
-    print(Doctor.formatDrInfo(headerList) + "\n")
-    valuesList = [self.id, self.name, self.specialization,
-                  self.workingTime, self.qualification, self.roomNumber]
-    print(Doctor.formatDrInfo(valuesList))
-
-
-def editDoctorInfo():
-    dr_Id = input(
-        "Please enter the id of the doctor that you want to edit their information:\n")
-    dr_index = Doctor.searchDoctorById(dr_Id)
-    if dr_index != -1:
-        drObjList = Doctor.readDoctorsFile()
-        drObjList[dr_index].name = input("Enter new Name: \n")
-        drObjList[dr_index].specialization = input(
-            "Enter new Specialist in: \n")
-        drObjList[dr_index].workingTime = input("Enter new Timing: \n")
-        drObjList[dr_index].qualification = input(
-            "Enter new Qualification: \n")
-        drObjList[dr_index].roomNumber = input("Enter new Room Number: \n")
-        Doctor.writeListOfDoctorsToFile(drObjList)
-    else:
-        return -1
-
-
-def displayDoctorsList():
-    path = "data\doctors.txt"
-    headerList = ["ID", "Name", "Specialty",
-                  "Timing", "Qualification", "Room Number"]
-    headerSpaces = [5, 23, 16, 16, 16, 12]
-    for item in headerList:
-        print(
-            item + (" " * (headerSpaces[headerList.index(item)] - len(item))), end="")
-    print("\n")
-    with open(path, 'r') as file:
-        lines = file.readlines()
-        for line in lines:
-            print(line)
-    file.close()
-
-
-def writeListOfDoctorsToFile(doctorsObjectList):
-    path = "data\doctors.txt"
-    file = open(path, "r+")
-    textOutput = ""
-    for dr in doctorsObjectList:
+        file = open(path, "a")
+        dr = drObject
         drProperties = [dr.id, dr.name, dr.specialization,
                         dr.workingTime, dr.qualification, dr.roomNumber]
-        ft = Doctor.formatDrInfo(drProperties)
-        textOutput += ft + "\n\n"
 
-    file.truncate(0)
-    file.write(textOutput)
-    file.close()
-
-
-def addDrToFile(drObject):
-    path = "data\doctors.txt"
-    textOutput = ""
-
-    file = open(path, "a")
-    dr = drObject
-    drProperties = [dr.id, dr.name, dr.specialization,
-                    dr.workingTime, dr.qualification, dr.roomNumber]
-
-    addText = Doctor.formatDrInfo(drProperties)
-    textOutput += addText + "\n\n"
-    file.write(textOutput)
-    file.close()
+        addText = Doctor.formatDrInfo(drProperties)
+        textOutput += addText + "\n\n"
+        file.write(textOutput)
+        file.close()
 
 
 class Facility:
@@ -397,3 +392,6 @@ class Patient:
         textOutput += addText + "\n\n"
         file.write(textOutput)
         file.close()
+
+
+Doctor.readDoctorsFile()
--- Management.py
+++ Management.py
@@ -1,48 +1,107 @@
-import Hospital as hs
+'''
+classesproject.py
+
+Written by Tyler Hynes, Coltan Rozander, and Evan Synnott-Hilliard
+
+This Program does things
+
+'''
+from Hospital import Doctor as Doc
+from Hospital import Laboratory as Lab
+from Hospital import Facility as Fac
+from Hospital import Patient as Pat
 
 
 def doctorMenu():
     runMenu = True
-    print("Doctors Menu: \n1 - Display Doctors list \n2 - Search for doctor by ID\n3 - Search for doctor by name \n4 - Add doctor \n5 - Edit doctor info \n6 - Back to the Main Menu")
-    userInput = input()
-    if userInput.isdigit() == False or int(userInput) != 1 and int(userInput) != 2 and int(userInput) != 3 and int(userInput) != 4 and int(userInput) != 5 and int(userInput) != 6:
-        print("Wrong Input, Try again: ")
-    elif int(userInput) == 1:
-        print("displaying Doctors List")
-        hs.displayDoctorsList
-    elif int(userInput) == 2:
-        docID = input("Enter the Doctor Id: ")
-        hs.searchDoctorById(docID)
-    elif int(userInput) == 3:
-        docName = input("Enter the Doctor name: ")
-        hs.searchDoctorByName(docName)
-    elif int(userInput) == 4:
-        hs.enterDrInfo()
-    elif int(userInput) == 5:
-        hs.editDoctorInfo
-    elif int(userInput) == 6:
-        runMenu = False
-        return
-'''
-def Patients:
+    while runMenu == True:
+        print("\nDoctors Menu: \n1 - Display Doctors list \n2 - Search for doctor by ID\n3 - Search for doctor by name \n4 - Add doctor \n5 - Edit doctor info \n6 - Back to the Main Menu")
+        userInput = input()
+        if userInput.isdigit() == False or int(userInput) != 1 and int(userInput) != 2 and int(userInput) != 3 and int(userInput) != 4 and int(userInput) != 5 and int(userInput) != 6:
+            print("Wrong Input, Try again: ")
+        elif int(userInput) == 1:
+            Doc.displayDoctorsList()
+        elif int(userInput) == 2:
+            docID = input("Enter the Doctor Id: ")
+            Doc.searchDoctorById(docID)
+        elif int(userInput) == 3:
+            docName = input("Enter the Doctor name: ")
+            Doc.searchDoctorByName(docName)
+        elif int(userInput) == 4:
+            Doc.enterDrInfo()
+        elif int(userInput) == 5:
+            docID = input("Enter the Doctor Id: ")
+            Doc.editDoctorInfo(docID)
+        elif int(userInput) == 6:
+            runMenu = False
+            return
 
-def Labatory:
+def FacMenu():
+    runMenu = True
+    while runMenu == True:
+        print("\nFacilities Menu:\n1 - Display Facilities list\n2 - Add Facility\n3 - Back to the Main Menu")
+        userInput = input()
+        if userInput.isdigit() == False or int(userInput) != 1 and int(userInput) != 2 and int(userInput) != 3:
+            print("Wrong Input, Try again: ")
+        elif int(userInput) == 1:
+            Fac.displayFacilities()
+        elif int(userInput) == 2:
+            newLab = input("Insert Name of laboratory")
+            Fac.addFacility()
+        elif int(userInput) == 3:
+            runMenu = False
+            return  
 
-def Facilities:
-'''
+def labMenu():
+    runMenu = True
+    while runMenu == True:
+        print("\nLaboratories Menu:\n1 - Display laboratories list\n2 - Add laboratory\n3 - Back to the Main Menu")
+        userInput = input()
+        if userInput.isdigit() == False or int(userInput) != 1 and int(userInput) != 2 and int(userInput) != 3:
+            print("Wrong Input, Try again: ")
+        elif int(userInput) == 1:
+            Lab.displayLabList()
+        elif int(userInput) == 2:
+            newLab = input("Insert Name of laboratory")
+            Lab.addLabToFile()
+        elif int(userInput) == 3:
+            runMenu = False
+            return
+
+def PatMenu():
+    runMenu = True
+    while runMenu == True:
+        print("\nPatients Menu: \n1 - Display patients list \n2 - Search for patient by ID\n3 - Add patient \n4 - Edit patient info \n5 - Back to the Main Menu")
+        userInput = input()
+        if userInput.isdigit() == False or int(userInput) != 1 and int(userInput) != 2 and int(userInput) != 3 and int(userInput) != 4 and int(userInput) != 5:
+            print("Wrong Input, Try again: ")
+        elif int(userInput) == 1:
+            Pat.displayPatientsList()
+        elif int(userInput) == 2:
+            patID = input("Enter the Patient Id: ")
+            Pat.searchPatientById(patID)
+        elif int(userInput) == 3:
+            Pat.enterPatientInfo
+        elif int(userInput) == 4:
+            docID = input("Enter the Doctor Id: ")
+            Pat.editPatientInfo(patID)
+        elif int(userInput) == 5:
+            runMenu = False
+            return
+
 runProgram = True
 while runProgram == True:
-    print("Welcome to Alberta Hospital (AH) Managment system \n Select from the following options, or select 0 to stop: \n1 - Doctors \n2 - Facilities\n3 - Laboratories \n4 - Patients")
+    print("\nWelcome to Alberta Hospital (AH) Managment system \nSelect from the following options, or select 0 to stop: \n1 - Doctors \n2 - Facilities\n3 - Laboratories \n4 - Patients")
     userInput = input()
     if userInput.isdigit() == False or int(userInput) != 1 and int(userInput) != 2 and int(userInput) != 3 and int(userInput) != 4 and int(userInput) != 0:
         print("Wrong Input, Try again")
     elif int(userInput) == 1:
         doctorMenu()
     elif int(userInput) == 2:
-        print()
+        FacMenu()
     elif int(userInput) == 3:
-        print()
+        labMenu()
     elif int(userInput) == 4:
-        print()
+        PatMenu()
     elif int(userInput) == 0:
         runProgram = False
